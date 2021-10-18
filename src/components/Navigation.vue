af5fd98a626b3bfa6ada9fa17469aa0bd075c3ba
Leon Husmann <git@leonhusmann.com>
2021-10-18 19:52:48 +0200

    add routing

---
+++ src/components/Navigation.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav>
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/sign-in">SignIn</router-link></li>
+      <li><router-link to="/sign-up">SignUp</router-link></li>
+    </ul>
+  </nav>
+</template>
