0fc527ac3fd22cbca7f215e9e1cc2d87a1b0c520
Leon Husmann <git@leonhusmann.com>
2021-10-18 20:00:42 +0200

    add some components

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,15 +2,21 @@
   <div>
     <h1>Dashboard</h1>
 
+    <Notification />
+
+    <Form />
+
     <Table />
   </div>
 </template>
 
 <script>
+import Notification from "../components/Notification.vue";
+import Form from "../components/Form.vue";
 import Table from "../components/Table.vue";
 
 export default {
   name: "Dashboard",
-  components: { Table },
+  components: { Notification, Form, Table },
 };
 </script>
